a9d00b232ed9ac45eccfda21f4804770db2fb053
Axelmc08 <155261379+Axelmc08@users.noreply.github.com>
2024-01-11 11:22:16 -0600

    Update registrarVendedor.php

--- ESCOMhub-master/registrarVendedor.php
+++ ESCOMhub-master/registrarVendedor.php
@@ -6,7 +6,7 @@
 
 		if(empty($_POST["nombrev"]) or empty($_POST["apellidov"]) or empty($_POST["boletav"]) or empty($_POST["userv"]) or
 			empty($_POST["correov"]) or empty($_POST["passwordv"]) or empty($_POST["pass2"]) or 
-			empty($_POST["telefonov"]) or empty($_POST["redsocialv"]) or empty($_POST["comida"]) or empty($_POST["golosinas"]) or empty($_POST["electronicaR"]) or empty($_POST["papeleriaR"]) or empty($_POST["serviciosR"]) or empty($_POST["ropa"]) or empty($_POST["videojuegos"]) or empty($_POST["libros"]) or empty($_POST["horaIn"]) or empty($_POST["horaFin"])){
+			empty($_POST["telefonov"]) or empty($_POST["redsocialv"]) or empty($_POST["comida"]) or empty($_POST["golosinas"]) or empty($_POST["electronicaR"]) or empty($_POST["papeleriaR"]) or empty($_POST["serviciosR"]) or empty($_POST["ropa"]) or empty($_POST["videojuegos"]) or empty($_POST["libros"]) or empty($_POST["postresR"]) or empty($_POST["bebidasR"]) or empty($_POST["horaIn"]) or empty($_POST["horaFin"])){
 
 			$mensaje = '<div class="alerta">Por favor, completa todos los campos</div>';
 
@@ -16,7 +16,7 @@
 			$mensaje = '<div class="alerta">El correo ya se encuentra registrado.</div>';
 		}elseif ($_POST["passwordv"] == $_POST["pass2"]){ 
     		require_once("conexion2.php");
-			$sql = $conexion2->prepare("INSERT INTO vendedorestabla (nombre,apellido,boleta, nomusuario,correo,password,telefono,redsocial, comida_preparada, golosinas_frituras, electronica, papeleria, servicios, ropa_accesorios, videojuegos_juguetes, libros_material, hora_inicio, hora_fin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			$sql = $conexion2->prepare("INSERT INTO vendedorestabla (nombre,apellido,boleta, nomusuario,correo,password,telefono,redsocial, comida_preparada, golosinas_frituras, electronica, papeleria, servicios, ropa_accesorios, videojuegos_juguetes, libros_material, postres, bebidas,hora_inicio, hora_fin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 	
 			$nombre=$_POST['nombrev'];
 			$apellido=$_POST['apellidov'];
@@ -34,13 +34,13 @@
 			$ropa_accesorios = $_POST['ropa'];
 			$videojuegos_juguetes = $_POST['videojuegos'];
 			$libros_material = $_POST['libros'];
+			$postres = $_POST['postresR'];
+			$bebidas = $_POST['bebidasR'];
 			$hora_inicio = $_POST['horaIn'];
 			$hora_fin = $_POST['horaFin'];
 
 
-			$sql->bind_param("ssssssssssssssssss", $nombre, $apellido, $boleta, $nomusuario, $correo, $password, $telefono, $redsocial, 
-				$comida_preparada, $golosinas_frituras, $electronica, $papeleria, $servicios, $ropa_accesorios, 
-				$videojuegos_juguetes, $libros_material, $hora_inicio, $hora_fin);
+			$sql->bind_param("ssssssssssssssssssss", $nombre, $apellido, $boleta, $nomusuario, $correo, $password, $telefono, $redsocial, $comida_preparada, $golosinas_frituras, $electronica, $papeleria, $servicios, $ropa_accesorios, $videojuegos_juguetes, $postres,$bebidas,$libros_material, $hora_inicio, $hora_fin);
 
 			if($sql->execute()) { 
 				echo '<script type="text/javascript">alert("¡Registro exitoso!");
