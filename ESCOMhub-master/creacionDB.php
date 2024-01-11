ba1a8c305fafa77849990940dfb3751c42df5eb0
Axelmc08 <155261379+Axelmc08@users.noreply.github.com>
2024-01-11 11:25:35 -0600

    Delete ESCOMhub-master directory

--- ESCOMhub-master/creacionDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("conexion.php");
-        $sql = "CREATE DATABASE escomhub";
-        if($conexion->query($sql) !== true){
-            die("Error al crear base de datos: " . $conexion->error);
-        }
-?>
